f00e87268aefa54ce378d96d79a3936d6c3dd4ac
beaudrot <beaudrot@umich.edu>
2017-10-17 11:20:06 -0400

    Editing for loop

--- do-uniq-species.sh
+++ do-uniq-species.sh
@@ -1,3 +1,7 @@
 # Purpose: Find the unique species from each dataset 
 # Date: Tuesday, October 17, 2017
 # Author: Lydia Beaudrot, beaudrot@umich.edu
+
+# Loop over all files
+
+for data_file in $@
